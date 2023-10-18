0aec278e365a9fababc13629f49f82ff95c5b1a1
Evgen <yahontov-sport@inbox.ru>
2023-10-18 16:43:25 +0300

    add file createsuperuser,createmamageruser,createtestuser

---
+++ users/management/commands/ctu.py
@@ -0,0 +1,18 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        user = User.objects.create(
+            email='test@testov.ru',
+            first_name='Test',
+            last_name='Testov',
+            is_staff=False,
+            is_superuser=False
+        )
+
+        user.set_password('123qwe123qwe')
+        user.save()
